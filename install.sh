4f3d287c7e561e3c843434f8459aa4292b4b44e1
Junoh Moon <snowphone@XPS-13.localdomain>
2018-08-28 12:35:00 +0900

    자주 쓰는 링크 ln

--- install.sh
+++ install.sh
@@ -11,7 +11,12 @@ ln -sf $(pwd)/.vimrc ~/.vimrc
 
 #sshkey 생성
 sudo ssh-keygen -A
-
+ 
+#링크 설정
+cd ~
+ln -s /mnt/c/Users/mjo97/OneDrive\ -\ 홍익대학교/ hongik
+ln -s /mnt/c/Users/mjo97/Downloads/
+ln -s /mnt/c/Users/mjo97/Dropbox/Documents/
 
 #apt 저장소를 국내로 변경
 sudo sed -i 's/kr.archive.ubuntu.com/ftp.daumkakao.com/g' /etc/apt/sources.list
