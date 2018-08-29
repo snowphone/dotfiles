173b733c2a73bdd6691e8f451326da1fcf5f1d19
Junoh Moon <snowphone@XPS-13.localdomain>
2018-08-29 22:10:16 +0900

    boost 라이브러리 추가

--- install.sh
+++ install.sh
@@ -28,7 +28,9 @@ sudo apt update -y
 sudo apt upgrade -y
 
 #필요한 프로그램 설치
-sudo apt install -y cmake clang clang-tidy clang-tools clang-format gcc gzip make vim python3 python3-dev python3-pip sshpass tmux unzip git zip w3m wget ctags
+sudo apt install -y ctags libboost-dev cmake clang clang-tidy clang-tools clang-format gcc \
+	python3 python3-dev python3-pip \
+	gzip make vim sshpass tmux unzip git zip w3m wget
 
 
 #vim 설정
