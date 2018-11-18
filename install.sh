3c216cb3e95a534fa315289e6b4061b572c2cc2c
Junoh Moon <snowphone@XPS-13.localdomain>
2018-11-18 12:17:23 +0900

    tmux resurrect 추가

--- install.sh
+++ install.sh
@@ -46,6 +46,9 @@ sudo apt install -y \
 
 #tmux 설정
 ln -fs $(folder)/.tmux.conf ~/
+#bind key + C-s, bind key + C-r을 이용해 전체 tmux session들을 저장 및 복구할 수 있다.
+git clone https://github.com/tmux-plugins/tmux-resurrect ~/.tmuxResurrect/
+
 
 #vim 설정
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
