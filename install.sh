50c45eb66072006e200c31c1e8dc380702114044
Moon Junoh <mjo970625@gmail.com>
2019-01-14 01:27:40 +0900

    install list: 자바 계열 제거, clang 및 git-extras 추가

--- install.sh
+++ install.sh
@@ -38,10 +38,9 @@ sudo apt update
 
 #필요한 프로그램 설치
 sudo apt install -y \
-	build-essential exuberant-ctags libboost-all-dev cmake clang-format \
+	build-essential clang exuberant-ctags libboost-all-dev cmake clang-format \
 	python3-dev python3 python3-dev python-pip python3-pip \
-	bear gzip make vim sshpass tmux unzip git zip w3m wget traceroute \
-	openjdk-8-jdk-headless gradle 
+	bear gzip make vim sshpass tmux unzip git zip w3m wget traceroute git-extras \
 
 
 #tmux 설정
