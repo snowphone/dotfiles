4c23e5afd919dd5e1af1d272639fd477c0bb9e8e
Junoh Moon <mjo970625@gmail.com>
2019-07-04 01:21:19 +0900

    Modify init commands

--- tmux-dev.sh
+++ tmux-dev.sh
@@ -1,6 +1,7 @@
 tmux -2  new -s dev \; \
   send-keys 'date | figlet | lolcat' C-m \; \
   split-window -v -p 35 \; \
+  send-keys 'while true; do date | figlet -w 200 | lolcat; sleep 0.8; done ' C-m \; \
   split-window -h -p 25 \; \
   send-keys 'htop -s PERCENT_CPU' C-m \; \
   select-pane -t 0 \;
