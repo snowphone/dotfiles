b546d1d5fb4c135905ad0edecfb70f3d913a15cf
Junoh Moon <mjo970625@gmail.com>
2020-04-16 22:44:52 +0900

    Add gdb

--- packages.sh
+++ packages.sh
@@ -105,7 +105,7 @@ fi
 
 if [[ $dist == "debian" ]]; then
 	pkgs=( \
-		build-essential less tar vim git gcc curl rename wget tmux make gzip zip unzip \
+		build-essential gdb less tar vim git gcc curl rename wget tmux make gzip zip unzip \
 		exuberant-ctags cmake clang-format \
 		python3-dev python3 python-pip python3-pip \
 		bfs tree htop \
