6ffcfae8b5732a8314a7126850c85c2a7a1187a9
Junoh Moon <mjo970625@gmail.com>
2020-05-03 03:44:25 +0900

    Make a symbolic link rather than configuring

--- install.sh
+++ install.sh
@@ -3,10 +3,9 @@
 # Include functions
 source $(pwd)/include.sh
 
-$(pwd)/packages.sh "$@"	|| die "Package installation failed"
+DEBIAN_FRONTEND=noninteractive $(pwd)/packages.sh "$@"	|| die "Package installation failed"
 $(pwd)/vim.sh			|| die "Vim configuration is failed"
 
-$(pwd)/git.sh			|| die "git configuration failed" # Global git configuration
 $(pwd)/misc.sh			|| die "sshd, transmission, and wsl folder aliasing are failed" # ssh server, transmission, wsl folder linking
 $(pwd)/ssh_key.sh		|| die "Failed to generate ssh key" # Generate ssh key
 $(pwd)/link.sh			|| die "Aliasing config files is failed" # symbolic links
