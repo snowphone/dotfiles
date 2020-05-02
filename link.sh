6d2aac42fb3ec2bd4aa61a79b466f8515c2b2d73
Junoh Moon <mjo970625@gmail.com>
2020-05-03 03:44:25 +0900

    Make a symbolic link rather than configuring

--- link.sh
+++ link.sh
@@ -3,6 +3,7 @@ folder=$(pwd)
 
 ln -fs "$folder"/.bashrc ~/.bashrc
 ln -fs "$folder"/.snapshot ~/.snapshot
+ln -fs "$folder"/.gitconfig  ~/.gitconfig
 
 mkdir ~/.pip
 ln -fs "$folder"/pip.conf ~/.pip/pip.conf
