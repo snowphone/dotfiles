3601143c0b3a6b6ea8602caa629ff39b39b28b06
Junoh Moon <mjo970625@gmail.com>
2020-05-13 21:06:16 +0900

    Edit panes position

--- tmux-dev.sh
+++ tmux-dev.sh
@@ -1,7 +1,8 @@
 tmux -2  new -s dev \; \
-  send-keys 'date | figlet | lolcat' C-m \; \
-  split-window -v -p 42 \; \
   send-keys 'screenfetch ' C-m \; \
-  split-window -h -p 35 \; \
+  split-window -h -p 65 \; \
+  send-keys 'date | figlet | lolcat' C-m \; \
+  select-pane -t 0 \; \
+  split-window -v -p 30 \; \
   send-keys 'htop -s PERCENT_CPU' C-m \; \
-  select-pane -t 0 \;
+  select-pane -t 2 \;
