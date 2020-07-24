a80a08f28d86c696597399f5cc1f614f2d70fe05
Junoh Moon <mjo970625@gmail.com>
2020-07-24 22:20:47 +0900

    Swap pane between 0 and 2 at the first time

--- tmux-dev.sh
+++ tmux-dev.sh
@@ -1,8 +1,10 @@
 tmux -2  new -s dev \; \
   send-keys 'screenfetch ' C-m \; \
   split-window -h -p 65 \; \
-  send-keys 'date | figlet | lolcat' C-m \; \
+  send-keys "curl wttr.in/Daejeon?2" C-m \; \
   select-pane -t 0 \; \
   split-window -v -p 30 \; \
   send-keys 'htop -s PERCENT_CPU' C-m \; \
+  select-pane -t 2 \; \
+  select-pane -t 0 \; \
   select-pane -t 2 \;
