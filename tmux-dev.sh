8cb8f7dde269da274975d837bcefa903a181d24c
Junoh Moon <mjo970625@gmail.com>
2020-07-24 22:33:07 +0900

    Use neofetch instead of screenfetch

--- tmux-dev.sh
+++ tmux-dev.sh
@@ -1,5 +1,5 @@
 tmux -2  new -s dev \; \
-  send-keys 'screenfetch ' C-m \; \
+  send-keys 'neofetch ' C-m \; \
   split-window -h -p 65 \; \
   send-keys "curl wttr.in/Daejeon?2" C-m \; \
   select-pane -t 0 \; \
