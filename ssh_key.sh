31a5e0cb4cb6806e82845f05b58e9fb0194d3b26
Junoh Moon <mjo970625@gmail.com>
2020-08-20 21:07:53 +0900

    Simplify ssh-copy-id procedure by copying public key into authorized_keys

--- ssh_key.sh
+++ ssh_key.sh
@@ -4,10 +4,13 @@ source ./include.sh
 
 if ls $HOME/.ssh &> /dev/null; then
 	die "$HOME/.ssh folder already exists. Please check and remove previously installed folder."
-else
-	ln -sf $(pwd)/.ssh $HOME/.ssh
-	chmod 600 ./.ssh/id_rsa
-	touch $(pwd)/.ssh/known_hosts
-	touch $(pwd)/.ssh/authorized_keys
 fi
+
+ln -sf $(pwd)/.ssh $HOME/.ssh
+chmod 600 ./.ssh/id_rsa
+touch $(pwd)/.ssh/known_hosts
+touch $(pwd)/.ssh/authorized_keys
+
+# Simplify ssh-copy-id procedure
+cat $HOME/.ssh/id_rsa.pub >> $HOME/.ssh/authorized_keys
 
