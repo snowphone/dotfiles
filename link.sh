75636d8e284efd95577c939090722ef0b9ac140f
Junoh Moon <mjo970625@gmail.com>
2020-12-07 14:22:09 +0900

    Extract some frequently used functions into files

--- link.sh
+++ link.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 folder=$(pwd)
 
+mkdir -p $HOME/.local/bin/
+
+ln -fs "$folder"/scripts/* $HOME/.local/bin/
+
 ln -fs "$folder"/.bashrc $HOME/.bashrc
 ln -fs "$folder"/.snapshot $HOME/.snapshot
 ln -fs "$folder"/.gitconfig  $HOME/.gitconfig
 
-mkdir $HOME/.pip
+mkdir -p $HOME/.pip
 ln -fs "$folder"/pip.conf $HOME/.pip/pip.conf
 
