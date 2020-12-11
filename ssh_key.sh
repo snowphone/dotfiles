e2205de5d5ea4220579d58cbb010de1cd249be43
Junoh Moon <mjo970625@gmail.com>
2020-12-11 11:03:15 +0900

    Trace script's location wherever it's called

--- ssh_key.sh
+++ ssh_key.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-source ./include.sh
+folder=$(dirname $0 | xargs realpath)
+
+source "$folder"/include.sh
 
 if ls $HOME/.ssh &> /dev/null; then
 	die "$HOME/.ssh folder already exists. Please check and remove previously installed folder."
 fi
 
-ln -sf $(pwd)/.ssh $HOME/.ssh
+ln -sf "$folder"/.ssh $HOME/.ssh
 chmod 600 ./.ssh/id_rsa
-touch $(pwd)/.ssh/known_hosts
-touch $(pwd)/.ssh/authorized_keys
+touch "$folder"/.ssh/known_hosts
+touch "$folder"/.ssh/authorized_keys
 
 # Simplify ssh-copy-id procedure
 cat $HOME/.ssh/id_rsa.pub >> $HOME/.ssh/authorized_keys
