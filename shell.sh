6842ee1f3dda30fa84090345d1e9de6bed4c91b5
Junoh Moon <mjo970625@gmail.com>
2021-01-30 18:05:04 +0900

    Use vim when opening files with xdg-open
    A shell script sdg-open checks whether the host has GUI display. If it
    gets cleared that the host only uses terminal, then this script invokes
    run-mailcap, another perl script. It reads mime.types to get its
    filetype, and soon reads its binded program by accessing .mailcap file.
    To update .mailcap file, use `update-mime --local` command.

--- shell.sh
+++ shell.sh
@@ -20,20 +20,7 @@ changeShell() {
 	$sudo sed -ie "$pattern" /etc/passwd
 }
 
-mkdir -p $HOME/.local/bin/
-
-ln -fs "$folder"/scripts/* $HOME/.local/bin/
-
-
-[ ! -f $HOME/.trueline.sh ] && curl https://raw.githubusercontent.com/petobens/trueline/master/trueline.sh -o $HOME/.trueline.sh
-ln -fs "$folder"/.bashrc $HOME/.bashrc			# Deprecated: bashrc
-ln -fs "$folder"/.snapshot $HOME/.snapshot		# For tmux prompt
-ln -fs "$folder"/.gitconfig  $HOME/.gitconfig	# Global git configuration
-ln -fs "$folder"/.zshrc $HOME/.zshrc
-ln -fs "$folder"/.p10k.zsh $HOME/.p10k.zsh
-
-mkdir -p $HOME/.pip
-ln -fs "$folder"/pip.conf $HOME/.pip/pip.conf
+"$folder"/link.sh
 
 if exists zsh; then
 	changeShell $(which zsh)
