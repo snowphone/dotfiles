2bf842227acf9db1bcb59517b3a1ba2cdda5ea27
Junoh Moon <mjo970625@gmail.com>
2021-02-10 22:02:16 +0900

    Use tpm, tmux package manager, and change tmux theme

--- tmux.sh
+++ tmux.sh
@@ -3,6 +3,9 @@
 folder=$(dirname $0 | xargs realpath)
 #tmux 설정
 ln -fs "$folder"/.tmux.conf $HOME/
-#bind key + C-s, bind key + C-r을 이용해 전체 tmux session들을 저장 및 복구할 수 있다.
-git clone https://github.com/tmux-plugins/tmux-resurrect $HOME/.tmuxResurrect/
 
+if [ ! -d ~/.tmux/plugins/tpm ]; then
+	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm && 
+		~/.tmux/plugins/tpm/bin/install_plugins && 
+		~/.tmux/plugins/tpm/bin/update_plugins all
+fi
