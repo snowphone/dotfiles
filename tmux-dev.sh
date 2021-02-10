df00ccba7dad8fe20cd17a3b714f8381aae1d955
Junoh Moon <mjo970625@gmail.com>
2021-02-11 01:19:19 +0900

    Change tmux design

--- tmux-dev.sh
+++ tmux-dev.sh
@@ -1,10 +1,10 @@
 tmux -2  new -s dev \; \
-  send-keys 'cat /run/motd.dynamic ' C-m \; \
+  send-keys 'btm || htop || top' C-m \; \
   split-window -h -p 65 \; \
   send-keys "neofetch" C-m \; \
   select-pane -t 0 \; \
   split-window -v -p 30 \; \
-  send-keys 'htop -s PERCENT_CPU' C-m \; \
+  clock-mode \; \
   select-pane -t 2 \; \
   select-pane -t 0 \; \
   select-pane -t 2 \;
