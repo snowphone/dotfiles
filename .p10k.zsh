5120ab1fc5765ea948ed1df36c417273350c275d
Junoh Moon <mjo970625@gmail.com>
2021-10-08 13:12:17 +0900

    Show CPU load

--- .p10k.zsh
+++ .p10k.zsh
@@ -102,9 +102,9 @@
     # ip                    # ip address and bandwidth usage for a specified network interface
     # public_ip             # public IP address
     # proxy                 # system-wide http/https/ftp proxy
-    # load                  # CPU load
     # ram                   # free RAM
     time                    # current time
+    load                  # CPU load
     battery               # internal battery
     # wifi                  # wifi speed
     # example               # example user-defined segment (see prompt_example function below)
