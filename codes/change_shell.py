98a3352c0218a59b0aa3a55570789d1430542f4a
Junoh Moon <mjo970625@gmail.com>
2021-11-23 22:03:49 +0900

    Implement shell.sh

--- codes/change_shell.py
+++ codes/change_shell.py
@@ -97,8 +97,46 @@ class FileLinker(Script):
 
 class ShellSwitcher(Script):
 	def run(self) -> None:
+		HOME = Path.home()
 
-		return super().run()
+		FileLinker(self.args).run()
+
+		if not self._exists("zsh"):
+			raise RuntimeError("zsh not exists")
+
+		ok, zsh_path, _ = self.shell.run("which zsh")
+		if not ok:
+			raise RuntimeError("Command 'which zsh' failed")
+		zsh_path = zsh_path.decode("utf-8").strip()
+
+		if os.environ["SHELL"] == zsh_path:
+			print("zsh is already a default shell")
+		else:
+			self._switch_to_zsh()
+			self.shell.exec_list(
+				"Running initialization commands in zsh",
+
+				f"zsh {HOME}/.zshrc",
+				f"zsh {HOME}/.zgenom/sources/init.zsh"
+			)
+
+		self.shell.exec(
+			"Installing nodejs via nvm",
+
+			f". {HOME}/.nvm/nvm.sh && nvm install node",
+		)
+
+		if self.args.typescript:
+			self.shell.exec(
+				"Installing typescript related things",
+
+				". {HOME}/.nvm/nvm.sh && npm install -g typescript ts-node pkg tslib"
+			)
+
+		return
+
+	def _exists(self, cmd: str) -> bool:
+		return self.shell.run(f"{cmd} --version")[0]
 
 	def _switch_to_zsh(self):
 		user = getuser()
@@ -107,7 +145,7 @@ class ShellSwitcher(Script):
 		ok, new_shell, _ = self.shell.run("which zsh")
 		if not ok:
 			raise RuntimeError("zsh is not installed")
-		new_shell = new_shell.decode("utf-8")
+		new_shell = new_shell.decode("utf-8").rstrip()
 
 		ok, lineNum, _ = self.shell.run(f"grep -n {user} /etc/passwd | cut -f1 -d:")
 		if not ok:
@@ -115,13 +153,16 @@ class ShellSwitcher(Script):
 		lineNum = int(lineNum.decode("utf-8"))
 
 		# | can be used as a separator in sed
-		pattern = f"{lineNum}s|{HOME}:.*$|{HOME}:{new_shell}|"
+		pattern = f"'{lineNum}s|{HOME}:.*$|{HOME}:{new_shell}|'"
 		self.shell.sudo_exec(
 			"Change default shell to zsh",
 			f"sed -ie {pattern} /etc/passwd"
 		)
 
 if __name__ == "__main__":
-	args = ArgumentParser().parse_args()
-	FileLinker(args).run()
+	parser = ArgumentParser()
+	parser.add_argument("--typescript", '-t', action='store_true')
+	args = parser.parse_args()
+
+	ShellSwitcher(args).run()
 
