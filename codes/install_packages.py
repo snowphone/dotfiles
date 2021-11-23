d7352803ce38868304d1e82a1c71919bd7020e11
Junoh Moon <mjo970625@gmail.com>
2021-11-23 22:04:16 +0900

    Rename folder

--- codes/install_packages.py
+++ codes/install_packages.py
@@ -4,18 +4,29 @@ from sys import exit
 
 from installer import make
 
+
 def main(args: Namespace):
 	installer = make(args)
 	installer.install()
 	return
 
-if __name__ == "__main__":
+
+def parse_args():
 	parser = ArgumentParser()
 
 	def add_flag(long: str, short: str, helpstr: str):
-		parser.add_argument(long, short, help=helpstr, action="store_true", default=False)
+		parser.add_argument(long,
+							short,
+							help=helpstr,
+							action="store_true",
+							default=False)
+		return
 
-	parser.add_argument("--dist", "-d", help="select distribution", choices=["debian", "redhat", "darwin"], required=True)
+	parser.add_argument("--dist",
+						"-d",
+						help="select distribution",
+						choices=["debian", "redhat", "darwin"],
+						required=True)
 
 	add_flag("--latex", "-l", "install texlive-full")
 	add_flag("--boost", "-b", "install libboost-all-dev")
@@ -24,6 +35,11 @@ if __name__ == "__main__":
 	add_flag("--golang", "-g", "install golang")
 	add_flag("--misc", "-m", "install some miscellaneous stuffs")
 	add_flag("--all", "-a", "enable all flags above")
-	args = parser.parse_args()
+
+	return parser.parse_args()
+
+
+if __name__ == "__main__":
+	args = parse_args()
 
 	exit(main(args))
