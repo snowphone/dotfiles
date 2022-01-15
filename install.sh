0123b2cdf78aff98ffa24dd562797c150cd6e8df
Junoh Moon <mjo970625@gmail.com>
2022-01-16 02:36:40 +0900

    Install requirements before executing commands

--- install.sh
+++ install.sh
@@ -3,5 +3,8 @@
 read -p 'Enter your password that encrypts your ssh key: ' SSH_PW
 export SSH_PW
 
+source <(curl -fsSL https://raw.githubusercontent.com/mkropat/sh-realpath/master/realpath.sh)
+
 proj_root=$(dirname $0 | xargs realpath)
+env python3 -m pip install --user -r $proj_root/requirements.txt
 $proj_root/codes/full_install.py $@
