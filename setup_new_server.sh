b934a9dfb722e09f4ea42aadfb1eab6641b20cdc
Junoh Moon <mjo970625@gmail.com>
2024-01-01 22:55:57 +0900

    Install dependencies and set up new server
    - Install fail2ban and xorg with `sudo apt-get install -y fail2ban xorg`
    - Install `macchina` with `cargo install macchina`
    - Install `CommitGPT` with `cargo install --git https://github.com/snowphone/CommitGPT`
    - Clone and install `cloudflare-cli` with `git clone https://github.com/snowphone/cloudflare-cli && cd cloudflare-cli && make install`
    - Create a symlink for `sshd.conf` to `sshd-61022.conf` in `/etc/fail2ban/filter.d` directory
    - Additional setup steps:
      - Restore crontab and backups created by crontab
      - Go to Code/typescript/add_music and link binary (`try cat makefile`)
      - Install X11 and Korean IME (kime)

---
+++ setup_new_server.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+sudo apt-get install -y fail2ban xorg
+cargo install macchina
+cargo install --git https://github.com/snowphone/CommitGPT
+git clone https://github.com/snowphone/cloudflare-cli && cd cloudflare-cli && make install
+
+cd /etc/fail2ban/filter.d && sudo ln -sf sshd.conf sshd-61022.conf
+
+
+
+cat <<'EOF'
+- Restore crontab and backups created by crontab.
+- Go to Code/typescript/add_music and link binary: (try cat makefile)
+- Install X11 and Korean IME (kime)
+EOF
