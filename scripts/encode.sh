71ae67d41be2c6aaae127deca56906940a30c853
Junoh Moon <mjo970625@gmail.com>
2024-01-01 21:24:47 +0900

    Create asset_strategy and encode.sh scripts
    The commit introduces two new scripts: `asset_strategy` and `encode.sh`. The `asset_strategy` script is a Python script that imports various modules, including `datetime` and `subprocess`, and runs a Docker command using `subprocess.run`. The `encode.sh` script is a shell script that takes an input file path as an argument and encodes it using the `linuxserver/ffmpeg` Docker image. The script uses various options and flags to specify the input and output files, as well as the desired encoding format.
    
    These scripts have been added to the repository in order to provide functionality related to asset strategy and video encoding.

---
+++ scripts/encode.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+case $1 in
+	'')
+		echo "No input given"
+		echo "Usage: $0 <file_path>"
+		exit 1
+		;;
+	-h|--help)
+		echo "Usage: $0 <file_path>"
+		exit 0
+		;;
+esac
+
+input="${1}"
+abspath="$(realpath "${1}")"
+folder="$(dirname "${abspath}")"
+basename="$(basename "${1}")"
+
+input="/data/${basename}"
+output="/data/${basename%.*}-encoded.mp4"
+
+echo "${input} -> ${output}"
+
+sudo docker run \
+	-it \
+	--rm \
+	--volume "${folder}:/data" \
+	--device /dev/dri:/dev/dri \
+	linuxserver/ffmpeg \
+	-hide_banner \
+	-v quiet \
+	-stats \
+	-y \
+	-hwaccel vaapi \
+	-vaapi_device /dev/dri/renderD128 \
+	-i "${input}" \
+	-c:v h264_vaapi \
+	-c:a copy \
+	-vf 'format=nv12|vaapi,hwupload' \
+	"${output}"
+
