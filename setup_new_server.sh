ea449ae9fa9f1f0d9994de5c95b0790d195a2018
Junoh Moon <mjo970625@gmail.com>
2024-01-06 11:04:11 +0900

    Install docker, clean up motd messages, install other packages, set timezone, configure fail2ban and sshd, and mention manual tasks.
    Changes:
    - Added commands to install docker
    - Removed unnecessary motd messages
    - Modified the esm and package alert output in apt_check.py
    - Updated esm status and motd updates
    - Installed additional packages (libssl-dev, molly-guard, network-manager)
    - Installed macchina and CommitGPT via cargo install and cloned cloudflare-cli
    - Set timezone to 'Asia/Seoul'
    - Installed fail2ban and configured sshd and fail2ban jails
    - Modified sshd_config to listen on port 61022
    - Added instructions for manual tasks: restoring crontab and backups, linking binary in the add_music directory, setting up X11 and Korean IME

--- setup_new_server.sh
+++ setup_new_server.sh
@@ -1,16 +1,78 @@
 #!/bin/sh
 
-sudo apt-get install -y fail2ban xorg libssl-dev
+set -e
+
+# Install docker
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo systemctl status docker
+sudo usermod -aG docker $USER
+newgrp docker
+docker run hello-world
+
+
+# esm 및 기타 이상한 motd 메시지 정리하기:
+sudo apt remove needrestart ubuntu-advantage-tools
+sudo chmod -x /etc/update-motd.d/50-motd-news
+sudo chmod -x /etc/update-motd.d/10-help-text
+sudo chmod -x /etc/update-motd.d/91-contract-ua-esm-status
+sudo chmod -x /etc/update-motd.d/98-reboot-required
+
+sudo sed -Ezi.orig \
+	-e 's/(def _output_esm_service_status.outstream, have_esm_service, service_type.:\n)/\1    return\n/' \
+	-e 's/(def _output_esm_package_alert.*?\n.*?\n.:\n)/\1    return\n/' \
+	/usr/lib/update-notifier/apt_check.py
+
+sudo /usr/lib/update-notifier/update-motd-updates-available --force
+sudo run-parts /etc/update-motd.d
+
+
+# Other
+sudo apt-get install -y \
+	libssl-dev \		# Prerequisite for commitgpt
+	molly-guard \		# Prevent accidental shutdown/reboot
+	network-manager \	# Wlan support
+	# xorg				# Full ubuntu-server image already includes X11 server
 cargo install macchina
 cargo install --git https://github.com/snowphone/CommitGPT
 git clone https://github.com/snowphone/cloudflare-cli && cd cloudflare-cli && make install
 
+timedatectl set-timezone 'Asia/Seoul'
+
+# Fail2ban and sshd
+sudo apt-get install -y fail2ban
 cd /etc/fail2ban/filter.d && sudo ln -sf sshd.conf sshd-61022.conf
+cat <<'EOF' | sudo tee /etc/fail2ban/jail.d/defaults-debian.conf
+[DEFAULT]
+bantime = 1h
+bantime.increment = true
 
+[sshd]
+enabled = true
+findtime = 1440m
+bantime = 525600m
+port = ssh
+mode = aggressive
 
+[sshd-61022]
+enabled = true
+findtime = 1440m
+bantime = 525600m
+port = 61022
+EOF
 
+sudo sed 's/(#\*)Port \d+/Port 61022\nPort 22/'
+
+if  ! grep 'Port 61022' /etc/ssh/sshd_config &> /dev/null ; then
+	sudo sed -i -r 's/(# *)?Port [0-9]+/Port 61022\nPort 22/' /etc/ssh/sshd_config
+fi
+
+# Manual things
 cat <<'EOF'
 - Restore crontab and backups created by crontab.
 - Go to Code/typescript/add_music and link binary: (try cat makefile)
-- Install X11 and Korean IME (kime)
+- Crontab
 EOF
