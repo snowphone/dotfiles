077b324ba65769fe8e125b536b511bda4d4d1936
Junoh Moon <mjo970625@gmail.com>
2024-02-12 20:08:55 +0900

    chore: Add instructions to substitute github protocol in install.sh
    The install.sh script was updated to include instructions on how to substitute a github protocol. This is necessary for users who want to use the git@github.com protocol instead of the https://github.com protocol.
    
    Changes:
    - Added instructions to substitute a github protocol in install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,9 @@
 
 proj_root=$( d=$(dirname $0); cd "$d" && pwd)
 $proj_root/code/full_install.py $@
+
+cat <<EOF
+Try the below to substitute a github protocol:
+
+sed -i 's|https://github.com/snowphone/dotfiles|git@github.com:snowphone/dotfiles|' ${proj_root}.git/config
+EOF
