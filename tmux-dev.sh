e47d928448feebcde4822494b9f3e51806536ce3
Junoh Moon <mjo970625@gmail.com>
2024-03-20 12:39:39 +0900

    Refactor tmux-dev.sh to use percentage values when splitting windows
    Changes:
    - Changed `split-window -h -p 65` to `split-window -h -l 65%`
    - Changed `split-window -v -p 30` to `split-window -v -l 30%`
    - Updated `send-keys` command to include `macchina`

--- tmux-dev.sh
+++ tmux-dev.sh
@@ -1,9 +1,9 @@
 tmux -2  new -s dev \; \
   send-keys 'gotop -a || htop || top' C-m \; \
-  split-window -h -p 65 \; \
-  send-keys "neofetch" C-m \; \
+  split-window -h -l 65% \; \
+  send-keys "macchina || neofetch" C-m \; \
   select-pane -t 0 \; \
-  split-window -v -p 30 \; \
+  split-window -v -l 30% \; \
   send-keys 'tty-clock -C1 -sc' C-m \; \
   select-pane -t 2 \; \
   select-pane -t 0 \; \
