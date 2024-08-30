84306761049f20ab87a8d843e3c829b0d3ab60f9
Junoh Moon <mjo970625@gmail.com>
2024-08-30 13:42:01 +0900

    Fix 7-zip extract command in `LinuxAMD64`
    Changes:
    - Add `tar_extract_flags='xJ'` to the `dl_cmd` method for extracting 7-zip tarball

--- code/linux_pkg.py
+++ code/linux_pkg.py
@@ -192,6 +192,7 @@ class LinuxAMD64(Script, GithubDownloadable):
             "Installing 7zip",
             self.dl_cmd(
             'https://7-zip.org/a/7z2408-linux-x64.tar.xz',
+            tar_extract_flags='xJ',
             ),
             f'rm -rf {self.HOME}/.local/bin/MANUAL {self.HOME}/.local/bin/readme.txt  {self.HOME}/.local/bin/History.txt {self.HOME}/.local/bin/License.txt {self.HOME}/.locall/bin/7zzs',
             f'mv -f {self.HOME}/.local/bin/7zz {self.HOME}/.local/bin/7z',
