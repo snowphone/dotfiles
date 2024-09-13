3031f1b391542fc1899d291c097bd1227e6d0d7f
Junoh Moon <mjo970625@gmail.com>
2024-09-13 23:32:05 +0900

    Improve script robustness by adding safety checks around variable expansion
    Changes:
    - Quoted variable expansion to handle pathnames with spaces.
    - Modified sed command to also match URLs with an optional user prefix for the protocol substitution.

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 proj_root=$(
-	d=$(dirname $0)
+	d=$(dirname "$0")
 	cd "$d" && pwd
 )
-$proj_root/code/full_install.py $@
+"$proj_root"/code/full_install.py "$@"
 
 cat <<EOF
 Try the below to substitute a github protocol:
 
-sed -i 's|https://github.com/snowphone/dotfiles|git@github.com:snowphone/dotfiles|' ${proj_root}/.git/config
+sed -Ei 's|https://(snowphone@)?github.com/snowphone/dotfiles|git@github.com:snowphone/dotfiles|' ${proj_root}.git/config
 EOF
