1f668566aa7363b3d55ae6f6d75de4ab5921760f
Junoh Moon <mjo970625@gmail.com>
2025-04-24 11:00:17 +0900

    refactor: Remove redundant karabiner configuration download in Darwin package manager
    Eliminate the download command for the karabiner configuration file
    within the DarwinPackageManager class. This code was redundant and
    related configuration setup should be handled elsewhere if necessary.

--- code/darwin_pkg.py
+++ code/darwin_pkg.py
@@ -141,11 +141,6 @@ class DarwinPackageManager(PackageManager):
         self.shell.exec("Updating PATH", f"printf '%s' '{path}' > {HOME}/.paths")
 
         self.shell.exec(
-            "Download karabiner configuration",
-            "curl -Lfo ~/.config/karabiner/karabiner.json https://gist.githubusercontent.com/snowphone/e28a836fe694a3e423ab42a37b99ba00/raw/karabiner.json",
-        )
-
-        self.shell.exec(
             "Aliasing python to python3",
             "ln -sf $(which python3) $(which python3 | sed 's/python3/python/')",
         )
