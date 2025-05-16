1e65ccc7a70e925c2614fb6a75d8c61e45122fa2
Junoh Moon <mjo970625@gmail.com>
2025-05-16 20:37:15 +0900

    Disable unstable autocompletion in install script
    Changes:
    Commented out the lines in `install_transcrypt.sh` script related to zsh autocompletion, as it is currently unstable.
    The autocompletion configurations are not being set up anymore to prevent potential issues.

--- install_transcrypt.sh
+++ install_transcrypt.sh
@@ -1,7 +1,8 @@
 
-autocomp="${HOME}/.local/share/zsh/vendor-completions"
-mkdir -p  "${autocomp}"
-curl -sfL -o "${autocomp}/_transcrypt" https://raw.githubusercontent.com/elasticdog/transcrypt/refs/heads/main/contrib/zsh/_transcrypt
+# Currenty autocompletion is not stable
+#autocomp="${HOME}/.local/share/zsh/vendor-completions"
+#mkdir -p  "${autocomp}"
+#curl -sfL -o "${autocomp}/_transcrypt" https://raw.githubusercontent.com/elasticdog/transcrypt/refs/heads/main/contrib/zsh/_transcrypt
 
 manpath="${HOME}/.local/man/man1"
 mkdir -p "${manpath}"
