9eb446d875f36205f3b0e8dbff1f803a3ecfe882
Junoh Moon <mjo970625@gmail.com>
2025-05-16 19:30:47 +0900

    Add installation of transcrypt and handle its configuration
    Changes:
    - Updated `install.sh` to export `PATH` early and use `git rev-parse` to find the project root.
    - Added installation of the `transcrypt` script in `~/.local/bin` including the manpage with a call to `install_transcrypt.sh`.
    - Added logic to check if `transcrypt` is installed and if not, configure with a user-provided password.
    - Created a new script `install_transcrypt.sh` to download and set up `transcrypt`, its zsh autocompletions, and manpage.

--- install.sh
+++ install.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
 
-proj_root=$(
-	d=$(dirname "$0")
-	cd "$d" && pwd
-)
-PATH=$HOME/.local/bin:$PATH
+
+export PATH=$HOME/.local/bin:$PATH
+
+proj_root="$(git rev-parse --show-toplevel)"
+
+# transcrypt script를 ~/.local/bin에 설치. manpage 포함
+"$proj_root/install_transcrypt.sh"
+
+# if transcrypt is not installed
+if ! transcrypt --display; then
+	printf "Enter password: "
+	read -r PASSWORD
+	# transcrypt가 레포지토리 안에 설정된다.
+	# 레포지토리 안에 이미 암호화된 파일이 있는 경우, 복호화도 진행한다.
+	transcrypt --cipher aes-256-cbc --password "$PASSWORD"
+fi
 
 "$proj_root"/code/full_install.py "$@"
 
---
+++ install_transcrypt.sh
@@ -0,0 +1,13 @@
+
+autocomp="${HOME}/.local/share/zsh/vendor-completions"
+mkdir -p  "${autocomp}"
+curl -sfL -o "${autocomp}/_transcrypt" https://raw.githubusercontent.com/elasticdog/transcrypt/refs/heads/main/contrib/zsh/_transcrypt
+
+manpath="${HOME}/.local/man/man1"
+mkdir -p "${manpath}"
+curl -sfL -o "${manpath}/transcrypt.1" https://raw.githubusercontent.com/elasticdog/transcrypt/refs/heads/main/man/transcrypt.1
+
+binpath="$HOME/.local/bin"
+mkdir -p "$binpath"
+curl -sfL -o "$binpath/transcrypt" https://raw.githubusercontent.com/elasticdog/transcrypt/refs/heads/main/transcrypt
+chmod +x "$binpath/transcrypt"
