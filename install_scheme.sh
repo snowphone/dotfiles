ac97f9f098a22398b77211535d9f0622ad38072e
Junoh Moon <mjo970625@gmail.com>
2025-05-31 11:41:20 +0900

    Remove unused install script
    Deleted the install_scheme.sh script as it is no longer required for installation.
    All previous installation steps for Chicken and related packages have been removed.
    Future installations should refer to updated documentation or automated processes.

--- install_scheme.sh
+++
@@ -1,9 +0,0 @@
-sudo apt install chicken-bin
-
-# Install lsp prerequisites
-chicken-install -s apropos chicken-doc srfi-18
-cd `csi -R chicken.platform -p '(chicken-home)'`
-curl http://3e8.org/pub/chicken-doc/chicken-doc-repo.tgz | sudo tar zx
-
-# Install lsp
-sudo chicken-install lsp-server
